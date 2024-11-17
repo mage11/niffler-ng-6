c9f34933acdb148ab3c31c9d8a089e39675ba7ee
mage <petya11@mail.ru>
2024-11-17 17:05:53 +0300

    HW-10.2 дз

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/converter/Browsers.java
@@ -0,0 +1,21 @@
+package guru.qa.niffler.jupiter.converter;
+
+import com.codeborne.selenide.SelenideConfig;
+
+public enum Browsers {
+    CHROME("chrome"),
+    FIREFOX("firefox");
+
+    private final String name;
+
+    Browsers(String name) {
+        this.name = name;
+    }
+
+    public SelenideConfig driverConfig() {
+        return new SelenideConfig()
+            .browser(name)
+            .timeout(5000L)
+            .pageLoadStrategy("eager");
+    }
+}
