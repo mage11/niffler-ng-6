86c905630a27798212c753e7762c6035c52dc8a3
Dmitrii Tuchs <dtuchs@gmail.com>
2025-01-09 19:26:23 +0300

    HW-15.2-PREP cherrypick prepare backends to graphql (qa-guru#67)

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/LoginTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/LoginTest.java
@@ -2,21 +2,19 @@ package guru.qa.niffler.test.web;
 
 import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.config.Config;
-import guru.qa.niffler.jupiter.annotation.meta.WebTest;
-import guru.qa.niffler.model.rest.UserJson;
 import guru.qa.niffler.page.LoginPage;
 import org.junit.jupiter.api.Test;
-@WebTest
-public class LoginTest {
 
-  private static final Config CFG = Config.getInstance();
+public class LogInTest {
 
-  @Test
-  void mainPageShouldBeDisplayedAfterSuccessLogin(UserJson user) {
-    Selenide.open(CFG.frontUrl(), LoginPage.class)
-        .login(user.username(), user.testData().password())
-        .headersBlocksShouldBeExists();
-  }
+    private static final Config CFG = Config.getInstance();
+    @Test
+    public void mainPageShouldBeDisplayedAfterSuccessLogin(){
+
+        Selenide.open(CFG.frontUrl(), LoginPage.class)
+            .login("PetyaMain", "123")
+            .headersBlocksShouldBeExists();
+    }
 
     @Test
     public void userShouldStayOnLoginPageAfterLoginWithBadCredentials(){
