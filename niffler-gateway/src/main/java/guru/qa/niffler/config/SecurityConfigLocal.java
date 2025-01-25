213156ce58c5541a3b7849c80d949a8db5f810f6
mage <petya11@mail.ru>
2025-01-25 16:13:26 +0300

    HW-15.2-PREP добавлен матчер для graphql

--- niffler-gateway/src/main/java/guru/qa/niffler/config/SecurityConfigLocal.java
+++ niffler-gateway/src/main/java/guru/qa/niffler/config/SecurityConfigLocal.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -36,7 +37,8 @@ public class SecurityConfigLocal {
                     antMatcher("/swagger-ui/**"),
                     antMatcher("/v3/api-docs/**"),
                     antMatcher("/actuator/health"),
-                    antMatcher("/graphiql/**")
+                    antMatcher("/graphiql/**"),
+                    antMatcher(HttpMethod.POST, "/graphql")
                 ).permitAll()
                 .anyRequest()
                 .authenticated()
