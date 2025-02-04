c840f4910608fdda534dd759983cfa98eb987e9b
mage <petya11@mail.ru>
2025-02-04 19:55:19 +0300

    HW-18.2 дз

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/UserdataSoapApi.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/UserdataSoapApi.java
@@ -1,7 +1,13 @@
 package guru.qa.niffler.api;
 
+
+import guru.qa.jaxb.userdata.AcceptInvitationRequest;
 import guru.qa.jaxb.userdata.AllUsersRequest;
 import guru.qa.jaxb.userdata.CurrentUserRequest;
+import guru.qa.jaxb.userdata.DeclineInvitationRequest;
+import guru.qa.jaxb.userdata.FriendsPageRequest;
+import guru.qa.jaxb.userdata.RemoveFriendRequest;
+import guru.qa.jaxb.userdata.SendInvitationRequest;
 import guru.qa.jaxb.userdata.UserResponse;
 import guru.qa.jaxb.userdata.UsersResponse;
 import retrofit2.Call;
@@ -25,5 +31,39 @@ public interface UserdataSoapApi {
   @POST("ws")
   Call<UsersResponse> allUsers(@Body AllUsersRequest allUsersRequest);
 
+  @Headers(value = {
+      "Content-type: text/xml",
+      "Accept-Charset: utf-8"
+  })
+  @POST("ws")
+  Call<UsersResponse> getFriends(@Body FriendsPageRequest request);
+
+  @Headers(value = {
+      "Content-type: text/xml",
+      "Accept-Charset: utf-8"
+  })
+  @POST("ws")
+  Call<Void> removeFriend(@Body RemoveFriendRequest request);
+
+  @Headers(value = {
+      "Content-type: text/xml",
+      "Accept-Charset: utf-8"
+  })
+  @POST("ws")
+  Call<UserResponse> sendInvitation(@Body SendInvitationRequest request);
+
+  @Headers(value = {
+      "Content-type: text/xml",
+      "Accept-Charset: utf-8"
+  })
+  @POST("ws")
+  Call<UserResponse> acceptInvitation(@Body AcceptInvitationRequest request);
+
+  @Headers(value = {
+      "Content-type: text/xml",
+      "Accept-Charset: utf-8"
+  })
+  @POST("ws")
+  Call<UserResponse> declineInvitation(@Body DeclineInvitationRequest request);
 
 }
