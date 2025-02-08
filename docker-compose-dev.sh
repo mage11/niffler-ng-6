7a35ba8c02ee0908df4a3f2023d41aa3fca03379
mage <petya11@mail.ru>
2025-02-08 23:10:47 +0300

    HW-19.1-PREP

--- docker-compose-dev.sh
+++ docker-compose-dev.sh
@@ -2,17 +2,6 @@
 source ./docker.properties
 export PROFILE=docker
 export PREFIX="${IMAGE_PREFIX}"
-export FRONT_VERSION="2.1.0"
-export ARCH=$(uname -m)
-
-echo '### Java version ###'
-java --version
-
-if [[ "$1" = "gql" ]]; then
-  export FRONT="niffler-ng-gql-client"
-else
-  export FRONT="niffler-ng-client"
-fi
 
 docker compose down
 
@@ -31,7 +20,7 @@ if [ ! -z "$docker_images" ]; then
 fi
 
 bash ./gradlew clean
-if [ "$1" = "push" ] || [ "$2" = "push" ]; then
+if [ "$1" = "push" ]; then
   echo "### Build & push images ###"
   bash ./gradlew jib -x :niffler-e-2-e-tests:test
   docker compose push frontend.niffler.dc
